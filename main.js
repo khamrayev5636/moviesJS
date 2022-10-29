6d377e7f1bd2255413e1eda89ca2e03d2264fa68
khamrayev5636 <khamrayev5636@gmail.com>
2022-10-29 17:15:24 +0500

    Initial commit

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const elList = document.querySelector(".movies__list");
 const elTemp = document.querySelector(".movies__temp").content;
+const elForm = document.querySelector(".form")
+const elSearch = document.querySelector(".input__search");
 const elFragment = document.createDocumentFragment();
 
 // Modal =====
@@ -16,59 +18,80 @@ const modalLink = document.querySelector(".modal-imdb-link")
 
 
 
+function greetingHour(time){
 
-function getDuration(time) {
-    
     const hours = Math.floor(time / 60);
     const minut = Math.floor(time % 60);
-    
-    return `${hours} hrs ${minut} min`
-    
+
+    return `${hours} hrs ${minut} min`;
 }
 
+function renderMovie(kino){
 
-function renderMovies(kino){
+    elList.textContent = "";
+
     
+
     kino.forEach(item => {
         
         const elClone = elTemp.cloneNode(true);
-        
-        elClone.querySelector(".movie-img").src = `https://i3.ytimg.com/vi/${item.ytid}/mqdefault.jpg`
+
+        elClone.querySelector(".movie-img").src =`https://i3.ytimg.com/vi/${item.ytid}/mqdefault.jpg`
         elClone.querySelector(".movies__title").textContent = item.Title;
         elClone.querySelector(".movies__rating").textContent = item.imdb_rating;
         elClone.querySelector(".movies__year").textContent = item.movie_year;
-        elClone.querySelector(".movies__runtime").textContent = getDuration(item.runtime);
-        elClone.querySelector(".movies__cat").textContent = item.Categories.split("|").join(", ");
+        elClone.querySelector(".movies__runtime").textContent = greetingHour(item.runtime);
+        elClone.querySelector(".movies__cat").textContent = item.Categories.split("|").join(", ")
         elClone.querySelector(".movies-btn").dataset.id = item.imdb_id;
+
         elFragment.appendChild(elClone);
-        
     });
     elList.appendChild(elFragment);
-}
+};
 
 
-function renderMoviesInfo(topilganKino){
-    
+function renderMovieInfo(topilganKino){
+
     modalTitle.textContent = topilganKino.Title;
     modalIframe.src = `https://www.youtube-nocookie.com/embed/${topilganKino.ytid}`;
     modalRating.textContent = topilganKino.imdb_rating;
     modalYear.textContent = topilganKino.movie_year;
-    modalRuntime.textContent = getDuration(topilganKino.runtime);
+    modalRuntime.textContent = greetingHour(topilganKino.runtime);
     modalCateg.textContent = topilganKino.Categories.split("|").join(", ");
     modalSumm.textContent = topilganKino.summary;
     modalLink.href = `https://www.imdb.com/title/${topilganKino.imdb_id}`;
-}
+};
 
-elList.addEventListener("click" ,function(evt){
-    const targetElement = evt.target;
-    if(targetElement.matches(".movies-btn")){
-        
-        // buttonning id attributining qiymatini olib, o'sha qiymatga ega bo'lgan kinoni topish
-        const btnId = targetElement.dataset.id;
-        const foundMovies = movies.find(movie => movie.imdb_id === btnId);
-        renderMoviesInfo(foundMovies);
-    }
+// Even Delegation
+
+elList.addEventListener("click" , function(evt){
+
+    if(evt.target.matches(".movies-btn")){
+
+        const btnId = evt.target.dataset.id;
+        const findMovie = movies.find(movie => movie.imdb_id === btnId);
+
+        renderMovieInfo(findMovie);
+    };
 });
+
+elForm.addEventListener("submit" , function(evt){
+    evt.preventDefault();
+
+    const  elSearchValue = elSearch.value.trim();
+
+    const elSearchReg = new RegExp(elSearchValue , "gi");
+
+    const findSearch = movies.filter(searchKino => String(searchKino.Title).match(elSearchReg));
+
+    renderMovie(findSearch);
+
+})
+
+
+
+renderMovie(movies);
+
 
 elModal.addEventListener("hide.bs.modal", function(){
     modalIframe.src = "";
@@ -76,5 +99,64 @@ elModal.addEventListener("hide.bs.modal", function(){
 
 
 
-renderMovies(movies.slice(0 , 33))
+
+
+// function getDuration(time) {
+
+//     const hours = Math.floor(time / 60);
+//     const minut = Math.floor(time % 60);
+
+//     return `${hours} hrs ${minut} min`
+
+// }
+
+
+// function renderMovies(kino){
+
+//     kino.forEach(item => {
+
+//         const elClone = elTemp.cloneNode(true);
+
+//         elClone.querySelector(".movie-img").src = `https://i3.ytimg.com/vi/${item.ytid}/mqdefault.jpg`
+//         elClone.querySelector(".movies__title").textContent = item.Title;
+//         elClone.querySelector(".movies__rating").textContent = item.imdb_rating;
+//         elClone.querySelector(".movies__year").textContent = item.movie_year;
+//         elClone.querySelector(".movies__runtime").textContent = getDuration(item.runtime);
+//         elClone.querySelector(".movies__cat").textContent = item.Categories.split("|").join(", ");
+//         elClone.querySelector(".movies-btn").dataset.id = item.imdb_id;
+//         elFragment.appendChild(elClone);
+
+//     });
+//     elList.appendChild(elFragment);
+// }
+
+
+// function renderMoviesInfo(topilganKino){
+
+//     modalTitle.textContent = topilganKino.Title;
+//     modalIframe.src = `https://www.youtube-nocookie.com/embed/${topilganKino.ytid}`;
+//     modalRating.textContent = topilganKino.imdb_rating;
+//     modalYear.textContent = topilganKino.movie_year;
+//     modalRuntime.textContent = getDuration(topilganKino.runtime);
+//     modalCateg.textContent = topilganKino.Categories.split("|").join(", ");
+//     modalSumm.textContent = topilganKino.summary;
+    // modalLink.href = `https://www.imdb.com/title/${topilganKino.imdb_id}`;
+// }
+
+// elList.addEventListener("click" ,function(evt){
+//     const targetElement = evt.target;
+//     if(targetElement.matches(".movies-btn")){
+
+//         // buttonning id attributining qiymatini olib, o'sha qiymatga ega bo'lgan kinoni topish
+//         const btnId = targetElement.dataset.id;
+//         const foundMovies = movies.find(movie => movie.imdb_id === btnId);
+//         renderMoviesInfo(foundMovies);
+//     }
+// });
+
+// elModal.addEventListener("hide.bs.modal", function(){
+//     modalIframe.src = "";
+// })
+
+// renderMovies(movies.slice(0 , 33));
 
